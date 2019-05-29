2a105aa7e3eb612a56c3d0d962356a8ad9a76f9f
jtertil <jtertil@gmail.com>
2019-05-29 11:42:53 +0200

    test1

--- case1/test_phonebook.py
+++ case1/test_phonebook.py
@@ -5,3 +5,9 @@ class PhonebookTest(unittest.TestCase):
 
     def test_create_phonebook(self):
         phonebook = Phonebook()
+
+    def test_lookup_entry_by_name(self):
+        phonebook = Phonebook()
+        phonebook.add("test1", "123456")
+        self.assertEqual("123456", phonebook.lookup("test1"))
+
