05308e4fb89dc12f8c35c8ff32d968a4066db012
jtertil <jtertil@gmail.com>
2019-05-29 12:28:47 +0200

    @unittest.skip

--- case1/test_phonebook.py
+++ case1/test_phonebook.py
@@ -11,3 +11,12 @@ class PhonebookTest(unittest.TestCase):
         phonebook.add("test1", "123456")
         self.assertEqual("123456", phonebook.lookup("test1"))
 
+    def test_missing_entry_raises_KeyError(self):
+        phonebook = Phonebook()
+        with self.assertRaises(KeyError):
+            phonebook.lookup("missing")
+
+    @unittest.skip("temp")
+    def test_empty_book_is_consistent(self):
+        phonebook = Phonebook()
+        self.assertTrue(phonebook.is_consistent())
