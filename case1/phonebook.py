924e30464c7703271ab647806651559c9a840316
jtertil <jtertil@gmail.com>
2019-05-29 14:03:43 +0200

    test-case-design

--- case1/phonebook.py
+++ case1/phonebook.py
@@ -7,3 +7,25 @@ class Phonebook:
 
     def lookup(self, name):
         return self.entries[name]
+
+    def get_names(self):
+        return self.entries.keys()
+
+    def get_numbers(self):
+        return self.entries.values()
+
+    def is_consistent(self):
+
+        # chceck for duplicate entries
+        if len(self.entries) != len(set(self.entries.values())):
+            return False
+
+        # chceck for numbers that prefix one another
+        for number in self.entries.values():
+            for value in self.entries.values():
+                if value == number:
+                    pass
+                elif value.startswith(number):
+                    return False
+        else:
+            return True
--- case1/test_phonebook.py
+++ case1/test_phonebook.py
@@ -3,20 +3,40 @@ from phonebook import Phonebook
 
 class PhonebookTest(unittest.TestCase):
 
-    def test_create_phonebook(self):
-        phonebook = Phonebook()
+    # setUp method -> runner will run that method prior to each test
+    def setUp(self):
+        self.phonebook = Phonebook()
+
+    # tearDown method -> runner will run that method after each test
 
     def test_lookup_entry_by_name(self):
-        phonebook = Phonebook()
-        phonebook.add("test1", "123456")
-        self.assertEqual("123456", phonebook.lookup("test1"))
+        self.phonebook.add("test1", "12345")
+        self.assertEqual("12345", self.phonebook.lookup("test1"))
 
     def test_missing_entry_raises_KeyError(self):
-        phonebook = Phonebook()
         with self.assertRaises(KeyError):
-            phonebook.lookup("missing")
+            self.phonebook.lookup("missing")
 
-    @unittest.skip("temp")
     def test_empty_book_is_consistent(self):
-        phonebook = Phonebook()
-        self.assertTrue(phonebook.is_consistent())
+        self.assertTrue(self.phonebook.is_consistent())
+
+    def test_phonebook_with_normal_entries_is_consistent(self):
+        self.phonebook.add("test1", "12345")
+        self.phonebook.add("test2", "67890")
+        self.assertTrue(self.phonebook.is_consistent())
+
+    def test_phonebook_with_duplicate_entries_is_inconsistent(self):
+        self.phonebook.add("test1", "12345")
+        self.phonebook.add("test2", "12345")
+        self.assertFalse(self.phonebook.is_consistent())
+
+
+    def test_phonebook_with_numbers_that_prefix_one_another_is_inconsistent(self):
+        self.phonebook.add("test1", "12345")
+        self.phonebook.add("test2", "123")
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_phonebook_adds_names_and_numbers(self):
+        self.phonebook.add("test1", "12345")
+        self.assertIn("test1", self.phonebook.get_names())
+        self.assertIn("12345", self.phonebook.get_numbers())
